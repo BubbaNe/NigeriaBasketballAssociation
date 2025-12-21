af56ed8c459057c6f6e833dea600f83bbf9c22b3
BubbaNe <minhlc.139@gmail.com>
2025-12-21 21:52:54 +0700

    Update phandangnhap.sql

--- phandangnhap.sql
+++ phandangnhap.sql
@@ -5,8 +5,5 @@ CREATE TABLE DangNhap (
     DangNhapID INT IDENTITY PRIMARY KEY,
     TenDangNhap VARCHAR(100) NOT NULL UNIQUE,
     MatKhau VARCHAR(255) NOT NULL,    --co the hash truoc de tang bao mat
-    TrangThai NVARCHAR(20) NOT NULL
-        CHECK (TrangThai IN (N'Hoạt động', N'Bị khóa')),
-    NgayTao DATETIME DEFAULT GETDATE()
 );
 GO
