7c57fa16f8256867e081954e6e0ad77f6611ff33
BubbaNe <minhlc.139@gmail.com>
2025-12-23 23:08:14 +0700

    Delete phandangnhap.sql

--- phandangnhap.sql
+++
@@ -1,9 +0,0 @@
-USE NigeriaBasketballAssociation;
-GO
-
-CREATE TABLE DangNhap (
-    DangNhapID INT IDENTITY PRIMARY KEY,
-    TenDangNhap VARCHAR(100) NOT NULL UNIQUE,
-    MatKhau VARCHAR(255) NOT NULL,    --co the hash truoc de tang bao mat
-);
-GO
